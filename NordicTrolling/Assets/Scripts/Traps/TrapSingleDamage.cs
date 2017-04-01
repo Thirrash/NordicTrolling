0f5334f3ab6421c3d33fecef8a4a15eaaa6a8fd4
MarcinJozwik <marty.jozwik@gmail.com>
2017-04-01 22:05:11 +0200

    New bear trap

--- NordicTrolling/Assets/Scripts/Traps/TrapSingleDamage.cs
+++ NordicTrolling/Assets/Scripts/Traps/TrapSingleDamage.cs
@@ -7,17 +7,24 @@ namespace Traps
 {
     public class TrapSingleDamage : TrapBase
     {
-        protected override void Start( ) {
-            base.Start( );
+        protected override void Start()
+        {
+            base.Start();
         }
 
-        protected override void OnCollisionEnter( Collision col ) {
-            base.OnCollisionEnter( col );
+        protected override void OnCollisionEnter(Collision col)
+        {
+            base.OnCollisionEnter(col);
 
-            if( col.gameObject.layer == ConstantsLayer.viking )
-                VikingSing.Inst.stats.TakeDamage( damageValue );
+            if (col.gameObject.layer == ConstantsLayer.viking)
+            {
+                VikingSing.Inst.stats.TakeDamage(damageValue);
+                GetComponent<Animator>().SetTrigger("Closing");
+                Destroy(gameObject.GetComponent<BoxCollider>());
+                Destroy(this);
+            }
 
-            Destroy( gameObject );
+            
         }
     }
 }
