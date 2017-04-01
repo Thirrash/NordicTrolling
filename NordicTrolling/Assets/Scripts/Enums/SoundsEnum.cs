6c36444ce2c8b678b6c80dd9b2c6da9eb54d3caf
RadekOlenski <radek.olenski@gmail.com>
2017-04-02 01:30:10 +0200

    More sfx enums

--- NordicTrolling/Assets/Scripts/Enums/SoundsEnum.cs
+++ NordicTrolling/Assets/Scripts/Enums/SoundsEnum.cs
@@ -3,6 +3,30 @@
     public class SoundsEnum
     {
         public static readonly string MenuButtonClick = "MenuButtonClick";
+        public static readonly string TrollBigSpawn = "TrollBigSpawn";
+        public static readonly string TrollDie1 = "TrollDie1";
+        public static readonly string TrollDie2 = "TrollDie2";
+        public static readonly string TrollDie3 = "TrollDie3";
+        public static readonly string TrollPatrolGo = "TrollPatrolGo";
+        public static readonly string TrollPatrolSpawn = "TrollPatrolSpawn";
+        public static readonly string TrollStandardSpawn = "TrollStandardSpawn";
+        public static readonly string VikingAhaahah = "VikingAhaahah";
+        public static readonly string VikingDie1 = "VikingDie1";
+        public static readonly string VikingDie2 = "VikingDie2";
+        public static readonly string VikingDie3 = "VikingDie3";
+        public static readonly string VikingInsane = "VikingInsane";
+        public static readonly string VikingLaugh1 = "VikingLaugh1";
+        public static readonly string VikingLaugh2 = "VikingLaugh2";
+        public static readonly string VikingNaPohybel = "VikingNaPohybel";
+        public static readonly string VikingTrollSpotted2 = "VikingTroll2";
+        public static readonly string VikingTrollSpotted = "VikingTrollSpotted";
+        public static readonly string VikingValhalla = "VikingValhalla";
+        public static readonly string VikingYhhyh = "VikingYhhyh";
+        public static readonly string LakeSplash = "LakeSplash";
+        public static readonly string BearTrap = "BearTrap";
+        public static readonly string AOEAttack = "AOEAttack";
+        public static readonly string TreeFall = "tree-fall";
+
     }
 
     public class MusicEnum
