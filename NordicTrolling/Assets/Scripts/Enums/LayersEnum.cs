1897d76c56561fa158368961b408bd7f134c2d87
RadekOlenski <radek.olenski@gmail.com>
2017-04-01 22:10:29 +0200

    Added TreeTrap prefab

--- NordicTrolling/Assets/Scripts/Enums/LayersEnum.cs
+++ NordicTrolling/Assets/Scripts/Enums/LayersEnum.cs
@@ -4,5 +4,6 @@
     {
         public static readonly string Default = "Default";       
         public static readonly string Troll = "Troll";
+        public static readonly string Viking = "Viking";
     }
 }
